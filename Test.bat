ad12400aeb5cea8b5e28fed679729868e7e246af
Phuwit1999 <phuwit.kha@gmail.com>
2026-04-20 00:21:57 +0700

    Delete Test.bat

--- Test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title DATA System
-:: ย้ายตำแหน่งไปยังโฟลเดอร์ที่เก็บไฟล์
-cd /d "C:\Users\ITSuperSupport\Desktop\CH"
-:: สั่งรัน streamlit ผ่าน python module
-python -m streamlit run TESTDATA.py
-pause
